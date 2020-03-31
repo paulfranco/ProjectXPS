3af84dda27f843e382a5610adcac5bde1fac19db
Paul Franco <paulfranco@me.com>
2020-03-30 23:27:51 -0700

    SignupActivity and User model

--- app/src/main/java/co/paulfran/projectxps/activities/SignUpActivity.kt
+++ app/src/main/java/co/paulfran/projectxps/activities/SignUpActivity.kt
@@ -2,11 +2,13 @@ package co.paulfran.projectxps.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
 import co.paulfran.projectxps.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +21,11 @@ class SignUpActivity : AppCompatActivity() {
         )
 
         setupActionBar()
+
+        // Click event for sign-up button.
+        btn_sign_up.setOnClickListener{
+            registerUser()
+        }
     }
 
     private fun setupActionBar() {
@@ -33,5 +40,48 @@ class SignUpActivity : AppCompatActivity() {
 
         // back button
         toolbar_sign_up_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    /**
+     * A function to validate the entries of a new user.
+     */
+    private fun validateForm(name: String, email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(name) -> {
+                showErrorSnackBar("Please enter name.")
+                false
+            }
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter email.")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter password.")
+                false
+            }
+            else -> {
+                true
+            }
+        }
+    }
+
+    /**
+     * A function to register a user to our app using the Firebase.
+     * For more details visit: https://firebase.google.com/docs/auth/android/custom-auth
+     */
+    private fun registerUser(){
+        val name: String = et_name.text.toString().trim { it <= ' ' }
+        val email: String = et_email.text.toString().trim { it <= ' ' }
+        val password: String = et_password.text.toString().trim { it <= ' ' }
+
+        // if returns true
+        if (validateForm(name, email, password)) {
+
+            Toast.makeText(
+                this@SignUpActivity,
+                "Now we can register a new user.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
