6a37e1c85318c0c9092ce7be3325a65f72fcba8d
Paul Franco <paulfranco@me.com>
2020-03-31 00:08:32 -0700

    implemented signin user functionality

--- app/src/main/java/co/paulfran/projectxps/activities/SignInActivity.kt
+++ app/src/main/java/co/paulfran/projectxps/activities/SignInActivity.kt
@@ -1,12 +1,16 @@
 package co.paulfran.projectxps.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
 import co.paulfran.projectxps.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +22,10 @@ class SignInActivity : AppCompatActivity() {
         )
 
         setupActionBar()
+
+        btn_sign_in.setOnClickListener {
+            signInRegisteredUser()
+        }
     }
 
     private fun setupActionBar() {
@@ -31,5 +39,57 @@ class SignInActivity : AppCompatActivity() {
         }
 
         toolbar_sign_in_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    /**
+     * A function for Sign-In using the registered user using the email and password.
+     */
+    private fun signInRegisteredUser() {
+        // Here we get the text from editText and trim the space
+        val email: String = et_email_signin.text.toString().trim { it <= ' ' }
+        val password: String = et_password_signin.text.toString().trim { it <= ' ' }
+
+        // if true
+        if (validateForm(email, password)) {
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            // Sign-In using FirebaseAuth
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+
+                        Toast.makeText(
+                            this@SignInActivity,
+                            "You have successfully signed in.",
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                        startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+                    } else {
+                        Toast.makeText(
+                            this@SignInActivity,
+                            task.exception!!.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    /**
+     * A function to validate the entries of a user.
+     */
+    private fun validateForm(email: String, password: String): Boolean {
+        return if (TextUtils.isEmpty(email)) {
+            showErrorSnackBar("Please enter email.")
+            false
+        } else if (TextUtils.isEmpty(password)) {
+            showErrorSnackBar("Please enter password.")
+            false
+        } else {
+            true
+        }
     }
 }
