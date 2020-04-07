250587f7f835742d186be3c18d08f5e471a66d0f
Paul Franco <paulfranco@me.com>
2020-04-07 07:29:13 -0700

    image chooser for the Profile Image

--- app/src/main/java/co/paulfran/projectxps/activities/MyProfileActivity.kt
+++ app/src/main/java/co/paulfran/projectxps/activities/MyProfileActivity.kt
@@ -1,13 +1,37 @@
 package co.paulfran.projectxps.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import co.paulfran.projectxps.R
 import co.paulfran.projectxps.firebase.FirestoreClass
 import co.paulfran.projectxps.models.User
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_my_profile.*
+import java.io.IOException
 
 class MyProfileActivity : BaseActivity() {
+
+    // Add a global variable for URI of a selected image from phone storage.
+    private var mSelectedImageFileUri: Uri? = null
+
+    /**
+     * A companion object to declare the constants.
+     */
+    companion object {
+        //A unique code for asking the Read Storage Permission using this we will be check and identify in the method onRequestPermissionsResult
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +40,24 @@ class MyProfileActivity : BaseActivity() {
         setupActionBar()
 
         FirestoreClass().loadUserData(this@MyProfileActivity)
+
+        iv_profile_user_image.setOnClickListener {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                showImageChooser()
+            } else {
+                /*Requests permissions to be granted to this application. These permissions
+                 must be requested in your manifest, they should not be granted to your app,
+                 and they should have protection level*/
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
     }
 
     /**
@@ -36,6 +78,57 @@ class MyProfileActivity : BaseActivity() {
     }
 
     /**
+     * This function will identify the result of runtime permission after the user allows or deny permission based on the unique code.
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+            //If permission is granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showImageChooser()
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(
+                    this,
+                    "Oops, you just denied the permission for storage. You can also allow it from settings.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == PICK_IMAGE_REQUEST_CODE
+            && data!!.data != null
+        ) {
+            // The uri of selection image from phone storage.
+            mSelectedImageFileUri = data.data
+            // Good idea to use try catch because something can go wrong
+            try {
+                // Load the user image in the ImageView.
+                Glide
+                    .with(this@MyProfileActivity)
+                    .load(Uri.parse(mSelectedImageFileUri.toString())) // URI of the image
+                    .centerCrop() // Scale type of the image.
+                    .placeholder(R.drawable.ic_user_place_holder) // A default place holder
+                    .into(iv_profile_user_image) // the view in which the image will be loaded.
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    /**
      * A function to set the existing details in UI.
      */
     fun setUserDataInUI(user: User) {
@@ -44,12 +137,25 @@ class MyProfileActivity : BaseActivity() {
             .load(user.image)
             .centerCrop()
             .placeholder(R.drawable.ic_user_place_holder)
-            .into(iv_user_image)
+            .into(iv_profile_user_image)
 
         et_name.setText(user.name)
         et_email.setText(user.email)
         if (user.mobile != 0L) {
             et_mobile.setText(user.mobile.toString())
         }
+    }
+
+    /**
+     * A function for user profile image selection from phone storage.
+     */
+    private fun showImageChooser() {
+        // An intent for launching the image selection of phone storage.
+        val galleryIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        // Launches the image selection of phone storage using the constant code.
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
     }
 }
