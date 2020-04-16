c1a6f4c165ae16fe8cbcbf65b871554cbf70b3c1
Paul Franco <paulfranco@me.com>
2020-04-16 04:29:54 -0700

    Load the card details and set the card title on the action bar

--- app/src/main/java/co/paulfran/projectxps/activities/CardDetailsActivity.kt
+++ app/src/main/java/co/paulfran/projectxps/activities/CardDetailsActivity.kt
@@ -3,11 +3,57 @@ package co.paulfran.projectxps.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import co.paulfran.projectxps.R
+import co.paulfran.projectxps.models.Board
+import co.paulfran.projectxps.utils.Constants
+import kotlinx.android.synthetic.main.activity_card_details.*
 
 class CardDetailsActivity : BaseActivity() {
+
+    private lateinit var mBoardDetails: Board
+    // A global variable for task item position
+    private var mTaskListPosition: Int = -1
+    // A global variable for card item position
+    private var mCardPosition: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_details)
+
+        getIntentData()
+
+        setupActionBar()
+    }
+
+    /**
+     * A function to setup action bar
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_card_details_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+
+            actionBar.title = mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name
+
+        }
+
+        toolbar_card_details_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    // A function to get all the data that is sent through intent.
+    private fun getIntentData() {
+
+        if (intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL) as Board
+        }
+        if (intent.hasExtra(Constants.TASK_LIST_ITEM_POSITION)) {
+            mTaskListPosition = intent.getIntExtra(Constants.TASK_LIST_ITEM_POSITION, -1)
+        }
+        if (intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)) {
+            mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
+        }
     }
 }
--- app/src/main/java/co/paulfran/projectxps/utils/Constants.kt
+++ app/src/main/java/co/paulfran/projectxps/utils/Constants.kt
@@ -25,6 +25,10 @@ object Constants {
 
     const val BOARD_DETAIL: String = "board_detail"
 
+    // Add all the required constants for passing the details to CardDetailsActivity through intent
+    const val TASK_LIST_ITEM_POSITION: String = "task_list_item_position"
+    const val CARD_LIST_ITEM_POSITION: String = "card_list_item_position"
+
     const val READ_STORAGE_PERMISSION_CODE = 1
 
     const val PICK_IMAGE_REQUEST_CODE = 2
