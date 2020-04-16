95981596c071a0ed9c15c70aa93938d50b42daeb
Paul Franco <paulfranco@me.com>
2020-04-16 02:33:44 -0700

    members UI

--- app/src/main/java/co/paulfran/projectxps/activities/MembersActivity.kt
+++ app/src/main/java/co/paulfran/projectxps/activities/MembersActivity.kt
@@ -1,7 +1,12 @@
 package co.paulfran.projectxps.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import co.paulfran.projectxps.R
 import co.paulfran.projectxps.adapters.MemberListItemsAdapter
@@ -10,6 +15,7 @@ import co.paulfran.projectxps.models.Board
 import co.paulfran.projectxps.models.User
 import co.paulfran.projectxps.utils.Constants
 import kotlinx.android.synthetic.main.activity_members.*
+import kotlinx.android.synthetic.main.dialog_search_member.*
 
 class MembersActivity : BaseActivity() {
 
@@ -50,6 +56,26 @@ class MembersActivity : BaseActivity() {
         toolbar_members_activity.setNavigationOnClickListener { onBackPressed() }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu to use in the action bar
+        menuInflater.inflate(R.menu.menu_add_member, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar menu items
+        when (item.itemId) {
+            R.id.action_add_member -> {
+
+
+                dialogSearchMember()
+
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     /**
      * A function to setup assigned members list into recyclerview.
      */
@@ -62,5 +88,34 @@ class MembersActivity : BaseActivity() {
 
         val adapter = MemberListItemsAdapter(this@MembersActivity, list)
         rv_members_list.adapter = adapter
+    }
+
+    /**
+     * Method is used to show the Custom Dialog.
+     */
+    private fun dialogSearchMember() {
+        val dialog = Dialog(this)
+        /*Set the screen content from a layout resource.
+    The resource will be inflated, adding all top-level views to the screen.*/
+        dialog.setContentView(R.layout.dialog_search_member)
+        dialog.tv_add.setOnClickListener(View.OnClickListener {
+
+            val email = dialog.et_email_search_member.text.toString()
+
+            if (email.isNotEmpty()) {
+                dialog.dismiss()
+            } else {
+                Toast.makeText(
+                    this@MembersActivity,
+                    "Please enter members email address.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+        dialog.tv_cancel.setOnClickListener(View.OnClickListener {
+            dialog.dismiss()
+        })
+        //Start the dialog and display it on screen.
+        dialog.show()
     }
 }
