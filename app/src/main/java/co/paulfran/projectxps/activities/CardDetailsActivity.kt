ec93b8a5e285d5be39902cde2de4ea9604996771
Paul Franco <paulfranco@me.com>
2020-04-16 09:33:44 -0700

    delete cards

--- app/src/main/java/co/paulfran/projectxps/activities/CardDetailsActivity.kt
+++ app/src/main/java/co/paulfran/projectxps/activities/CardDetailsActivity.kt
@@ -1,11 +1,17 @@
 package co.paulfran.projectxps.activities
 
+import android.app.Activity
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import co.paulfran.projectxps.R
+import co.paulfran.projectxps.firebase.FirestoreClass
 import co.paulfran.projectxps.models.Board
+import co.paulfran.projectxps.models.Card
+import co.paulfran.projectxps.models.Task
 import co.paulfran.projectxps.utils.Constants
 import kotlinx.android.synthetic.main.activity_card_details.*
 
@@ -28,6 +34,15 @@ class CardDetailsActivity : BaseActivity() {
         // Set the card name in the EditText for editing.
         et_name_card_details.setText(mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name)
         et_name_card_details.setSelection(et_name_card_details.text.toString().length) // The cursor after the string length
+
+        // Add a click event for update button and also call the function to update the card details.)
+        btn_update_card_details.setOnClickListener {
+            if(et_name_card_details.text.toString().isNotEmpty()) {
+                updateCardDetails()
+            }else{
+                Toast.makeText(this@CardDetailsActivity, "Enter card name.", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 
@@ -60,7 +75,7 @@ class CardDetailsActivity : BaseActivity() {
         // Handle presses on the action bar menu items
         when (item.itemId) {
             R.id.action_delete_card -> {
-
+                alertDialogForDeleteCard(mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name)
                 return true
             }
         }
@@ -79,5 +94,90 @@ class CardDetailsActivity : BaseActivity() {
         if (intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)) {
             mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
         }
+    }
+
+    /**
+     * A function to get the result of add or updating the task list.
+     */
+    fun addUpdateTaskListSuccess() {
+
+        hideProgressDialog()
+
+        setResult(Activity.RESULT_OK)
+        finish()
+    }
+
+    /**
+     * A function to update card details.
+     */
+    private fun updateCardDetails() {
+
+        // Here we have updated the card name using the data model class.
+        val card = Card(
+            et_name_card_details.text.toString(),
+            mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].createdBy,
+            mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].assignedTo
+        )
+
+        // Here we have assigned the update card details to the task list using the card position.
+        mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition] = card
+
+        // Show the progress dialog.
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().addUpdateTaskList(this@CardDetailsActivity, mBoardDetails)
+    }
+
+    /**
+     * A function to show an alert dialog for the confirmation to delete the card.
+     */
+    private fun alertDialogForDeleteCard(cardName: String) {
+        val builder = AlertDialog.Builder(this)
+        //set title for alert dialog
+        builder.setTitle(resources.getString(R.string.alert))
+        //set message for alert dialog
+        builder.setMessage(
+            resources.getString(
+                R.string.confirmation_message_to_delete_card,
+                cardName
+            )
+        )
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+        //performing positive action
+        builder.setPositiveButton(resources.getString(R.string.yes)) { dialogInterface, which ->
+            dialogInterface.dismiss() // Dialog will be dismissed
+            // Call the function to delete the card.
+            deleteCard()
+
+        }
+        //performing negative action
+        builder.setNegativeButton(resources.getString(R.string.no)) { dialogInterface, which ->
+            dialogInterface.dismiss() // Dialog will be dismissed
+        }
+        // Create the AlertDialog
+        val alertDialog: AlertDialog = builder.create()
+        // Set other dialog properties
+        alertDialog.setCancelable(false) // Will not allow user to cancel after clicking on remaining screen area.
+        alertDialog.show()  // show the dialog to UI
+    }
+
+    /**
+     * A function to delete the card from the task list.
+     */
+    private fun deleteCard() {
+
+        // Here we have got the cards list from the task item list using the task list position.
+        val cardsList: ArrayList<Card> = mBoardDetails.taskList[mTaskListPosition].cards
+        // Here we will remove the item from cards list using the card position.
+        cardsList.removeAt(mCardPosition)
+
+        val taskList: ArrayList<Task> = mBoardDetails.taskList
+        taskList.removeAt(taskList.size - 1)
+
+        taskList[mTaskListPosition].cards = cardsList
+
+        // Show the progress dialog.
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().addUpdateTaskList(this@CardDetailsActivity, mBoardDetails)
     }
 }
